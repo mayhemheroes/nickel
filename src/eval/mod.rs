b9ddd53a984c62a63daa690085bddc237fcbe6eb
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2022-06-24 16:10:13 +0200

    Add %array_lazy_assume% operation

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -626,7 +626,7 @@ where
                     .collect();
                 Closure {
                     body: RichTerm::new(
-                        Term::Array(closurized_array, ArrayAttrs { closurized: true }),
+                        Term::Array(closurized_array, attrs.clone().as_closurized()),
                         pos,
                     ),
                     env: local_env,
