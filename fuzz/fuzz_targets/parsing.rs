458cef88cf35883baf85192f1f55922181814fb3
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-06-02 20:32:29 +0000

    Fix docker build

--- fuzz/fuzz_targets/parsing.rs
+++ fuzz/fuzz_targets/parsing.rs
@@ -1,9 +1,23 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
+use std::io::Cursor;
+
+use nickel_lang_core::{
+    eval::cache::CacheImpl,
+    program::{Program, ProgramBuilder},
+    typecheck::TypecheckMode,
+};
+
 fuzz_target!(|data: &[u8]| {
-    if let Ok(mut program) = nickel_lang::program::Program::new_from_source(data, "fuzz") {
-        _ = program.typecheck();
-        _ = program.output_doc(&mut std::io::sink());
+    let result: Result<Program<CacheImpl>, _> = ProgramBuilder::new()
+        .add_source(Cursor::new(data.to_vec()), "fuzz")
+        .build();
+
+    if let Ok(mut program) = result {
+        let _ = program.typecheck(TypecheckMode::Walk);
+        if let Ok(doc) = program.extract_doc() {
+            let _ = doc.write_markdown(&mut std::io::sink());
+        }
     }
 });
